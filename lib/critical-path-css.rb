2f53b1476cc43d0b4bcb910b3d5f845d3eba7863
Michael Misshore <michael@mudbugmedia.com>
2015-09-17 16:18:02 -0500

    Need self. for module

--- lib/critical-path-css.rb
+++ lib/critical-path-css.rb
@@ -4,7 +4,7 @@ module CriticalPathCss
   CACHE_NAMESPACE = 'critical-path-css'
   PENTHOUSE_PATH = "#{File.dirname(__FILE__)}/penthouse/penthouse.js"
 
-  def generate_critical_css (main_css_path, base_url, routes)
+  def self.generate (main_css_path, base_url, routes)
     full_main_css_path = "#{Rails.root.to_s}/public#{main_css_path}"
 
     routes.each do |route|
@@ -13,7 +13,7 @@ module CriticalPathCss
     end
   end
 
-  def fetch_critical_css (route)
+  def self.fetch (route)
     Rails.cache.read(route, namespace: CACHE_NAMESPACE) || ''
   end
 end
