ab0a3e396e52e591e56f8d63a6cdea3e159bc328
Michael Misshore <michael@mudbugmedia.com>
2015-10-01 20:42:38 -0500

    Generate configuration yaml file through the install generator

--- lib/generators/critical_path_css/install_generator.rb
+++ lib/generators/critical_path_css/install_generator.rb
@@ -9,5 +9,11 @@ module CriticalPathCss
       task_filename = 'critical_path_css.rake'
       copy_file "../../tasks/#{task_filename}", "lib/tasks/#{task_filename}"
     end
+
+    # Copy the needed configuration YAML file for generating critical CSS
+    def copy_config_file
+      task_filename = 'critical_path_css.yml'
+      copy_file "../../config/#{task_filename}", "config/#{task_filename}"
+    end
   end
 end
