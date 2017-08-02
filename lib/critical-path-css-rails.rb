5a627d8a07ec4d2ba3e1678b85c815b90e27f5bf
Michael Misshore <michael@mudbugmedia.com>
2017-08-02 09:50:40 -0500

    Fix namespacing

--- lib/critical-path-css-rails.rb
+++ lib/critical-path-css-rails.rb
@@ -6,7 +6,7 @@ module CriticalPathCss
   CACHE_NAMESPACE = 'critical-path-css'
 
   def self.generate(route)
-    Rails.cache.write(
+    ::Rails.cache.write(
       route,
       CssFetcher.new(config).fetch_route(route),
       namespace: CACHE_NAMESPACE,
@@ -16,20 +16,20 @@ module CriticalPathCss
 
   def self.generate_all
     CssFetcher.new(config).fetch.each do |route, css|
-      Rails.cache.write(route, css, namespace: CACHE_NAMESPACE, expires_in: nil)
+      ::Rails.cache.write(route, css, namespace: CACHE_NAMESPACE, expires_in: nil)
     end
   end
 
   def self.clear(route)
-    Rails.cache.delete(route, namespace: CACHE_NAMESPACE)
+    ::Rails.cache.delete(route, namespace: CACHE_NAMESPACE)
   end
 
   def self.clear_matched(routes)
-    Rails.cache.delete_matched(routes, namespace: CACHE_NAMESPACE)
+    ::Rails.cache.delete_matched(routes, namespace: CACHE_NAMESPACE)
   end
 
   def self.fetch(route)
-    Rails.cache.read(route, namespace: CACHE_NAMESPACE) || ''
+    ::Rails.cache.read(route, namespace: CACHE_NAMESPACE) || ''
   end
 
   def self.config
--- lib/critical_path_css/rails/config_loader.rb
+++ lib/critical_path_css/rails/config_loader.rb
@@ -1,11 +1,11 @@
 module CriticalPathCss
   module Rails
-    module ConfigLoader
+    class ConfigLoader
       CONFIGURATION_FILENAME = 'critical_path_css.yml'
 
       def load
-        config = YAML.load(ERB.new(File.read(configuration_file_path)).result)[Rails.env]
-        config['css_path'] = "#{Rails.root}/public" + (
+        config = YAML.load(ERB.new(File.read(configuration_file_path)).result)[::Rails.env]
+        config['css_path'] = "#{::Rails.root}/public" + (
             config['css_path'] ||
               ActionController::Base.helpers.stylesheet_path(
                 config['manifest_name'], host: ''
@@ -17,7 +17,7 @@ module CriticalPathCss
       private
 
       def configuration_file_path
-        @configuration_file_path ||= Rails.root.join('config', CONFIGURATION_FILENAME)
+        @configuration_file_path ||= ::Rails.root.join('config', CONFIGURATION_FILENAME)
       end
     end
   end
