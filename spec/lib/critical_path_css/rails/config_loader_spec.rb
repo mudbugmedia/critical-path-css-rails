378a1289dd89f5f14770798befba0673961e376d
Michael Misshore <michael@mudbugmedia.com>
2019-03-03 06:43:31 -0600

    Move configs into fixtures

--- spec/lib/critical_path_css/rails/config_loader_spec.rb
+++ spec/lib/critical_path_css/rails/config_loader_spec.rb
@@ -9,21 +9,7 @@ RSpec.describe 'ConfigLoader' do
     end
 
     context 'when single css_path is specified' do
-      let(:config_file) {
-        <<~CONFIG
-          defaults: &defaults
-            base_url: http://0.0.0.0:9292
-            css_path: /test.css
-            routes:
-              - /
-
-          development:
-            <<: *defaults
-
-          test:
-            <<: *defaults
-        CONFIG
-      }
+      let(:config_file) { file_fixture('config/single-css-path.yml').read }
 
       it 'sets css_path with the path' do
         expect(subject.config['css_path']).to eq '/app/spec/internal/public/test.css'
@@ -35,24 +21,7 @@ RSpec.describe 'ConfigLoader' do
     end
 
     context 'when multiple css_paths are specified' do
-      let(:config_file) {
-        <<~CONFIG
-          defaults: &defaults
-            base_url: http://0.0.0.0:9292
-            css_paths:
-              - /test.css
-              - /test2.css
-            routes:
-              - /
-              - /new_route
-
-          development:
-            <<: *defaults
-
-          test:
-            <<: *defaults
-        CONFIG
-      }
+      let(:config_file) { file_fixture('config/mutliple-css-paths.yml').read }
 
       it 'sets css_path to empty string' do
         expect(subject.config['css_path']).to eq ''
@@ -64,21 +33,7 @@ RSpec.describe 'ConfigLoader' do
     end
 
     context 'when no paths are specified' do
-      let(:config_file) {
-        <<~CONFIG
-          defaults: &defaults
-            base_url: http://0.0.0.0:9292
-            manifest_name: application
-            routes:
-              - /
-
-          development:
-            <<: *defaults
-
-          test:
-            <<: *defaults
-        CONFIG
-      }
+      let(:config_file) { file_fixture('config/no-paths-specified.yml').read }
 
       it 'sets css_path with the path' do
         expect(subject.config['css_path']).to eq '/stylesheets/application.css'
@@ -90,25 +45,7 @@ RSpec.describe 'ConfigLoader' do
     end
 
     context 'when single css_path and multiple css_paths are both specified' do
-      let(:config_file) {
-        <<~CONFIG
-          defaults: &defaults
-            base_url: http://0.0.0.0:9292
-            css_path: /test.css
-            css_paths:
-              - /test.css
-              - /test2.css
-            routes:
-              - /
-              - /new_route
-
-          development:
-            <<: *defaults
-
-          test:
-            <<: *defaults
-        CONFIG
-      }
+      let(:config_file) { file_fixture('config/paths-both-specified.yml').read }
 
       it 'raises an error' do
         expect { subject }.to raise_error LoadError, 'Cannot specify both css_path and css_paths'
@@ -116,25 +53,7 @@ RSpec.describe 'ConfigLoader' do
     end
 
     context 'when css_paths and routes are not the same length' do
-      let(:config_file) {
-        <<~CONFIG
-          defaults: &defaults
-            base_url: http://0.0.0.0:9292
-            css_paths:
-              - /test.css
-              - /test2.css
-            routes:
-              - /
-              - /new_route
-              - /newer_route
-
-          development:
-            <<: *defaults
-
-          test:
-            <<: *defaults
-        CONFIG
-      }
+      let(:config_file) { file_fixture('config/paths-and-routes-not-same-length.yml').read }
 
       it 'raises an error' do
         expect { subject }.to raise_error LoadError, 'Must specify css_paths for each route'
